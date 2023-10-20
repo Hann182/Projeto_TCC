de9037deb1974a44bdd930ec6075a627db8c166d
Mateus Mendes <mateus.mendes@it4solution.com>
2023-10-20 03:35:22 -0300

    Add project

---
+++ Repositories/Cadastrar.py
@@ -0,0 +1,44 @@
+import typing
+from PyQt5.QtWidgets import QApplication, QWidget, QPushButton
+from PyQt5.Qt import Qt
+from PyQt5.QtCore import pyqtSlot
+from PyQt5 import QtCore, QtGui, QtWidgets
+from PyQt5.QtGui import QIcon, QPixmap
+from PyQt5.QtWidgets import *
+from PyQt5.QtPrintSupport import *
+import os, sys
+
+sys.path.insert(1, './view')
+
+from View.Cadastro import *
+from banco.query import sqlite_db
+
+class Cadastro(QDialog):
+    def __init__(self, *args, **argvs):
+        super(Cadastro,self).__init__(*args, **argvs)
+        self.ui = Ui_cadastro()
+        self.ui.setupUi(self)
+        self.ui.btnConfirma.clicked.connect(self.add)
+        self.ui.btnLimpar.clicked.connect(self.limpa)
+
+    def add(self):
+        db = sqlite_db("./repositories/usuarios_cadastrados.db")
+       
+        name = self.ui.imName.text()
+        passd = self.ui.imPass.text()
+        passc = self.ui.imPassC.text()
+
+        if name=='' or passc == '' or passd == '':
+            QMessageBox.warning(QMessageBox(), 'Alerta!','Campo(s) vazio(s)')
+        elif passc != passd:
+            QMessageBox.warning(QMessageBox(), 'Alerta!','Senhas divergentes')
+        else:
+            db.crud("INSERT INTO usuarios (nome, senha) VALUES('{}', '{}')".format(name, passd))
+            QMessageBox.information(QMessageBox(), 'Cadastro', 'Cadastro realizada')
+
+    def limpa(self):
+        self.ui.imName.setText('')
+        self.ui.imPass.setText('')
+        self.ui.imPassC.setText('')
+
+    
---
+++ Repositories/Logar.py
@@ -0,0 +1,56 @@
+import typing
+from PyQt5.QtWidgets import QApplication, QWidget, QPushButton
+from PyQt5.Qt import Qt
+from PyQt5.QtCore import pyqtSlot
+from PyQt5 import QtCore, QtGui, QtWidgets
+from PyQt5.QtGui import QIcon, QPixmap
+from PyQt5.QtWidgets import *
+from PyQt5.QtPrintSupport import *
+import os, sys
+
+sys.path.insert(1, './view')
+
+from View.TelaLogin import *
+from Cadastrar import Cadastro
+from banco.query import sqlite_db
+from View.Paint import Window
+
+class Login(QDialog):
+    def __init__(self, *args, **argvs):
+        super(Login, self).__init__(*args, **argvs)
+        self.ui = Ui_Login()
+        self.ui.setupUi(self)
+        self.ui.btnEntrar.clicked.connect(self.login)
+        self.ui.btnCadasto.clicked.connect(self.cadastro)
+        self.ui.btnApagar.clicked.connect(self.limpar)
+        
+    def login(self):
+        db = sqlite_db("usuarios_cadastrados.db")
+
+        user = self.ui.imName.text()
+        passd = self.ui.ImPassword.text()
+        if user == '' or passd == '':
+            QMessageBox.warning(QMessageBox(), 'Alerta!','Campo(s) vazio(s), por favor preencha!')
+        else:
+            dados = db.dados("SELECT * from usuarios WHERE nome = '{}' and senha = '{}'".format(user, passd))
+            if dados:
+                QMessageBox.information(QMessageBox(), 'Login Realizado', 'Bem vindo ' + user)
+                self.window = Window()
+                self.window.show()
+                self.hide()
+            else:
+                QMessageBox.information(QMessageBox(), 'Login invalido', 'Senha ou usuario incorreto')
+
+    def limpar(self):
+        self.ui.imName.clear()
+        self.ui.ImPassword.clear()
+
+    def cadastro(self):
+        self.window = Cadastro()
+        self.window.show()
+
+app = QApplication(sys.argv)
+if (QDialog.Accepted == True):
+    window = Login()
+    window.show()
+sys.exit(app.exec_())
